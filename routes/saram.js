57e4d7644c51993d03af4e424c2707a976a2dd4a
kimyerimweb <kimyerimweb@gmail.com>
2022-02-22 19:05:56 +0900

    feat: Create job request function

--- routes/saram.js
+++ routes/saram.js
@@ -1,40 +1,64 @@
-const express = require("express");
-const router = express.Router();
-const axios = require("axios");
-require("dotenv").config();
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+require('dotenv').config()
 const headers = {
-  Accept: "application/json",
-};
+  Accept: 'application/json',
+}
+const it = require('../dataTable/IT')
 
-const URL = `https://oapi.saramin.co.kr/job-search?access-key=${process.env.API_KEY}`;
+const URL = `https://oapi.saramin.co.kr/job-search?access-key=${process.env.API_KEY}`
 
-router.route("/:id").get((req, res) => {
-  const { id } = req.params;
-  const url = URL + `&id=${id}`;
+router.route('/:id').get((req, res) => {
+  const { id } = req.params
+  const url = URL + `&id=${id}`
 
   axios({
-    method: "GET",
+    method: 'GET',
     url,
     headers,
-  }).then((response) => res.json(response.data));
-});
+  }).then((response) => res.json(response.data))
+})
 
-router.route("/").post((req, res) => {
-  const { keywords, loc_cd, sort } = req.body;
+router.route('/job/:job').get((req, res) => {
+  const { job } = req.params
+  let code = null
+
+  it.forEach((el) => {
+    if (el.name === job) {
+      code = el.code
+    }
+  })
+
+  if (!code) {
+    res.status(404).send('존재하지 않는 직무입니다.')
+  }
+
+  const url = URL + `&job_cd=${code}`
+
+  axios({
+    method: 'GET',
+    url,
+    headers,
+  }).then((response) => res.json(response.data))
+})
+
+router.route('/').post((req, res) => {
+  const { keywords, loc_cd, sort } = req.body
 
   const url =
     URL +
-    (keywords ? `&keywords=${encodeURI(keywords)}` : "") +
-    (loc_cd ? `&loc_cd=${loc_cd}` : "") +
-    (sort ? `&sort=${sort}` : "");
+    (keywords ? `&keywords=${encodeURI(keywords)}` : '') +
+    (loc_cd ? `&loc_cd=${loc_cd}` : '') +
+    (sort ? `&sort=${sort}` : '')
 
-  console.log(url);
+  console.log(url)
 
   axios({
-    method: "GET",
+    method: 'GET',
     url,
     headers,
-  }).then((response) => res.json(response.data));
-});
+  }).then((response) => res.json(response.data))
+})
 
-module.exports = router;
+module.exports = router
